66d743dabf9bc48188cd8124de45181e060343ee
Anna Thornton <athornton@pivotal.io>
2017-04-14 17:57:21 -0400

    WIP; unit spec checking env vars is wrong
    [#143585835]

--- cf_spec/unit/appdynamics_install_spec.rb
+++ cf_spec/unit/appdynamics_install_spec.rb
@@ -9,133 +9,71 @@ describe "Appdynamics Installer" do
   let(:buildpack_dir)           { File.join(File.expand_path(File.dirname(__FILE__)), '..', '..') }
   let(:build_dir)               { Dir.mktmpdir }
   let(:app_name)                { 'unit-test-app' }
-  let(:tier_name)               { 'tier-name' }
-  let(:node_name)               { 'node-name' }
-  let(:host_name)               { '1.1.1.1' }
-  let(:port)                    { '1234' }
-  let(:account_name)            { 'customer' }
-  let(:account_access_key)      { 'fffff-fffff-fffff-fffff' }
   let(:profile_d_appdynamics)   { File.join(build_dir, '.profile.d', 'appdynamics-setup.sh') }
+  let(:old_bp_dir)              { ENV['BP_DIR'] }
+  let(:old_vcap_application)    { ENV['VCAP_APPLICATION'] }
+  let(:old_vcap_services)       { ENV['VCAP_SERVICES'] }
+  let(:old_host_name)           { ENV['appdynamics_controller_host_name'] }
+  let(:old_port)                { ENV['appdynamics_controller_port'] }
+  let(:old_account)             { ENV['appdynamics_agent_account_name'] }
+  let(:old_ssl)                 { ENV['appdynamics_controller_ssl_enabled'] }
+  let(:old_key)                 { ENV['appdynamics_agent_account_access_key'] }
+  let(:old_app)                 { ENV['appdynamics_agent_application_name'] }
+  let(:old_tier)                { ENV['appdynamics_agent_tier_name'] }
+  let(:old_prefix)              { ENV['appdynamics_agent_node_name_prefix'] }
 
   before do
     ENV["BP_DIR"] = buildpack_dir
       vcap_application =  {
         "application_name"=> app_name
       }
-    ENV["VCAP_APPLICATION"] = vcap_application.to_json
+
+    ENV['VCAP_APPLICATION'] = vcap_application.to_json
   end
 
   after do
     FileUtils.rm_rf(build_dir) if defined? build_dir
-    ENV["VCAP_APPLICATION"] = nil
+    ENV['BP_DIR'] = old_bp_dir
+    ENV['vcap_application'] = old_vcap_application
+    ENV['vcap_services'] = old_vcap_services
+    ENV['appdynamics_controller_host_name'] = old_host_name
+    ENV['appdynamics_controller_port'] = old_port
+    ENV['appdynamics_agent_account_name'] = old_account
+    ENV['appdynamics_controller_ssl_enabled'] = old_ssl
+    ENV['appdynamics_agent_account_access_key'] = old_key
+    ENV['appdynamics_agent_application_name'] = old_app
+    ENV['appdynamics_agent_tier_name'] = old_tier
+    ENV['appdynamics_agent_node_name_prefix'] = old_prefix
   end
 
   subject do
+    vcap_services = "{\"elephantsql\":[{}],\"appdynamics\":[{\"credentials\":{\"host-name\":\"test-host\",\"port\":\"1234\",\"account-name\":\"test-account\",\"ssl-enabled\":\"true\",\"account-access-key\":\"test-key\"}}]}"
     Dir.chdir(buildpack_dir) do
-      @stdout, @stderr, @status = Open3.capture3("lib/vendor/appdynamics/install.sh #{build_dir}")
+      @stdout, @stderr, @status = Open3.capture3("VCAP_SERVICES=\"#{vcap_services} profile/appdynamics-setup.sh")
     end
   end
 
-  context 'vcap services' do
-    before do
-      ENV["VCAP_SERVICES"] = vcap_services.to_json
-    end
+  # context 'vcap_services is not present in environment' do
+  #   let(:vcap_services) { "{}" }
 
-    after do
-      ENV["VCAP_SERVICES"] = nil
-    end
+  #   it 'does not create .profile.d/appdynamics-setup.sh file' do
+  #     subject
+  #     expect(@status).to be_success
+  #   end
+  # end
 
-    context 'contains appdynamics' do
-      let(:vcap_services) {
-        {
-          "appdynamics"=>[{
-            "credentials"=> {
-              "host-name"=> "test-host-name",
-              "port"=> "1234",
-              "account-name"=> "test-customer",
-              "account-access-key"=> "appdynamics_license_key_set_by_service_binding"
-            }
-          }]
-        }
-      }
-
-      context 'APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY and APPDYNAMICS_AGENT_ACCOUNT_NAME not set' do
-        it "sets the APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY variable from VCAP_SERVICES" do
-          subject
-          expect(@status).to be_success
-          profile_d_contents = File.read(profile_d_appdynamics)
-          expect(profile_d_contents).to include('export APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=appdynamics_license_key_set_by_service_binding')
-        end
-
-        it "sets the APPDYNAMICS_AGENT_ACCOUNT_NAME variable" do
-          subject
-          expect(@status).to be_success
-          profile_d_contents = File.read(profile_d_appdynamics)
-          expect(profile_d_contents).to include('export APPDYNAMICS_AGENT_ACCOUNT_NAME=test-customer')
-        end
-      end
-
-      context 'APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY set' do
-        before do
-          ENV["APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY"] = "license_key_in_env_var"
-        end
-
-        after do
-          ENV["APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY"] = nil
-        end
-
-        it "sets the APPDYNAMICS_AGENT_APPLICATION_NAME variable" do
-          subject
-          expect(@status).to be_success
-          profile_d_contents = File.read(profile_d_appdynamics)
-          expect(profile_d_contents).to include('export APPDYNAMICS_AGENT_APPLICATION_NAME=unit-test-app')
-        end
-      end
-
-      context 'APPDYNAMICS_AGENT_APPLICATION_NAME set' do
-        before do
-          ENV["APPDYNAMICS_AGENT_APPLICATION_NAME"] = "unit-test-app"
-        end
-
-        after do
-          ENV["APPDYNAMICS_AGENT_APPLICATION_NAME"] = nil
-        end
-
-        it "sets the APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY variable from VCAP_SERVICES" do
-          subject
-          expect(@status).to be_success
-          profile_d_contents = File.read(profile_d_appdynamics)
-          expect(profile_d_contents).to include('export APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=appdynamics_license_key_set_by_service_binding')
-        end
-      end
-
-      context 'APPDYNAMICS_AGENT_APPLICATION_NAME and APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY set' do
-        before do
-          ENV["APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY"] = "appdynamics_license_key_set_by_service_binding"
-          ENV["APPDYNAMICS_AGENT_APPLICATION_NAME"] = "unit-test-app"
-        end
-
-        after do
-          ENV["APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY"] = nil
-          ENV["APPDYNAMICS_AGENT_APPLICATION_NAME"] = nil
-        end
-      end
-    end
-
-    context 'VCAP_SERVICES is not present in environment' do
-      let(:vcap_services) { {} }
-
-      subject do
-        Dir.chdir(buildpack_dir) do
-          @stdout, @stderr, @status = Open3.capture3("unset VCAP_SERVICES; lib/vendor/appdynamics/install.sh #{build_dir}")
-        end
-      end
-
-      it 'does not create .profile.d/appdynamics-setup.sh file' do
-        subject
-        expect(@status).to be_success
-        expect(File.exist?(profile_d_appdynamics)).to be_falsey
-      end
+  context 'vcap services contains appdynamics' do
+    it "sets the appropriate environment variables from VCAP_SERVICES AppDynamocs credentials" do
+      subject
+      expect(@status).to be_success
+      expect(ENV['APPDYNAMICS_CONTROLLER_HOST_NAME']).to equal('test-host-name')
+      expect(ENV['APPDYNAMICS_CONTROLLER_PORT']).to equal('1234')
+      expect(ENV['APPDYNAMICS_AGENT_ACCOUNT_NAME']).to equal('test-customer')
+      expect(ENV['APPDYNAMICS_CONTROLLER_SSL_ENABLED']).to equal('true')
+      expect(ENV['APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY']).to equal('appdynamics_license_key_set_by_service_binding')
+      expect(ENV['APPDYNAMICS_AGENT_APPLICATION_NAME']).to equal('unit-test-app')
+      expect(ENV['APPDYNAMICS_AGENT_TIER_NAME']).to equal('unit-test-app')
+      expect(ENV['APPDYNAMICS_AGENT_NODE_NAME_PREFIX']).to equal('unit-test-app')
     end
   end
 end
